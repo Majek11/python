c997f8a1505a4867eba37e74cb0b2a2401f50066
Majek <majeklekan9@gmail.com>
2024-12-03 05:12:53 -0500

    python-class

---
+++ Classwork/practice.py
@@ -0,0 +1,19 @@
+print("Welcome to python, Lighthouse!!!")
+
+number = 3
+
+number = "Babe"
+
+char = 'M'
+
+print(type(number))
+
+radius = int(input("Enter your radius: "))
+
+area = 3.142 * radius ** 2
+
+print("The area of circle with radius", radius, "is", area)
+
+name = input("Enter your name: ")
+
+print(len(name))
