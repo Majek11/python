17b9ab886276219461dfc5c82bb2b73a24a0e69b
Majek11 <majeklekan9@gmail.com>
2025-01-15 15:48:11 +0100

    phase-gate

--- PhaseGateOne/functiontwonumbers.py
+++ PhaseGateOne/functiontwonumbers.py
@@ -27,16 +27,4 @@ def two_numbers():
 
     
 
- public int multiplyfeature ( int digit, int multiply ) {
-    int total = 0;
-    for ( int i = 0; i <= multiply; i++ ) {
-      total *= digit; 
-    }
-    return total;
-    
-    
- public void testMultiplyFeature() {
-    SimpleCalculator myCalculator = new SimpleCalculator();
-    int multiply = myCalculator.multiplyfeature(2, 5);
-    assertEquals(10, multiply);
-  }
+
